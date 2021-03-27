58858ff29c9b1a34efb36bac8d1c58a701e0cb37
Anders Gustafsson <andersgee@gmail.com>
2021-03-27 15:07:11 +0100

    clsx and a border style for debugging

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,13 @@
 import * as React from "react";
 import * as styles from "./card.module.scss";
+import { border } from "@styles/border.module.scss";
+import clsx from "clsx";
 
 export default function Card() {
-  return <div className={styles.card}>Card here</div>;
+  return (
+    <div className={clsx(styles.card, border)}>
+      <div>card image</div>
+      <div>card text</div>
+    </div>
+  );
 }
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import * as styles from "./cards.module.scss";
 import Card from "@components/Card";
+import { border } from "@styles/border.module.scss";
+import clsx from "clsx";
 
 export default function Cards() {
   return (
-    <div className={styles.cards}>
+    <div className={clsx(styles.cards, border)}>
+      <h1>Our Latest Works</h1>
       <Card />
       <Card />
       <Card />
