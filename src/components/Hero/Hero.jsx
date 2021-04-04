f9504ead33837a431f084c21ba5ca5e86ec070c6
andy <andersgee@gmail.com>
2021-04-04 18:20:25 +0200

    ok

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import * as styles from "./hero.module.scss";
 export default function Hero() {
   return (
     <div className={styles.placeholderhero}>
-      <img className={styles.logo} src="/images/logo/andyfx.png" />
+      <img className={styles.logo} src="/images/logo/andyfx.png" alt="andyfx" />
       <h1 className={styles.andyfx}>Andyfx</h1>
 
       {/*<div></div>*/}
